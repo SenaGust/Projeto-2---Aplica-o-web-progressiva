9571471564055fcb2d298635a4538ed9c348014c
Gustavo Sena <44535224+SenaGust@users.noreply.github.com>
2021-10-02 01:56:52 +0000

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+// Define o nome do cache atual, considerando a sua versão.
+var cacheName = 'app-topicos-v1.0';
+
+// Armazena todos os arquivos no cache atual
+self.addEventListener('install', function (event) {
+  caches.open(cacheName).then((cache) => {
+    cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.webmanifest',
+      'readme.md',
+      'script.js',
+      'style.css'
+    ]);
+  });
+});
+
+
+// Recupera todos os nomes de cache e apaga aqueles
+// que forem diferentes do cache atual
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+// Tenta servir o arquivo do cache atual. Se não for possível,
+// baixa o recurso da web e o armazena localmente, antes de entregar
+// uma cópia para o usuário.
+self.addEventListener('fetch', function (event) {
+  let resposta = caches.open(cacheName).then((cache) => {
+    return cache.match(event.request).then((recurso) => {
+      if (recurso) return recurso;
+      return fetch(event.request).then((recurso) => {
+        cache.put(event.request, recurso.clone());
+        return recurso;
+      });
+    });
+  });
+  event.respondWith(resposta);
+});
